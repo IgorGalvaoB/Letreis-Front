dc53c6c85e09e23568848aff660c8ec6b904fec2
Igor Galvao <igorgalvaob@gmail.com>
2022-09-04 19:27:02 -0300

    FilledSquare done

--- src/components/ui/AnimatedItem.js
+++ src/components/ui/AnimatedItem.js
@@ -4,93 +4,107 @@ import EmptySquare from './EmptySquare.js';
 import InputSquare from './InputSquare.js';
 import FilledSquare from './FilledSquare.js'
 
-const AnimatedItem = ({ value,flipAnimated, key, delay, stage, successAnimated}) => {
+const AnimatedItem = ({ value, flipAnimated, id, stage, successAnimated }) => {
 
-    const delayTime = key*0.7 
+    const delayTime = id * 0.7
+    const animated = flipAnimated || successAnimated
+
     
-    const flipAnimation  = keyframes`
-        0% {
-            -webkit-transform: rotateX(0deg);
-            transform: rotateX(0deg);
-        },
-        100%{
-            -webkit-transform: rotateX(180deg);
-            transform: rotateX(180deg);
-        },
+    const flipAnimation = keyframes`
+    
+    0% {
+        -webkit-transform: rotateX(0deg);
+        transform: rotateX(0deg);
+    },
+    100%{
+        -webkit-transform: rotateX(180deg);
+        transform: rotateX(180deg);
+    },
     `
-
+    
     const successAnimation = keyframes`
-
-        0%{
-            -webkit-transform: translateY(0px);
-            transform: translateY(0px);
-        }
-        50%{
-            -webkit-transform: translateY(20px);
-            transform: translateY(20px);
-        }
-        100%{
-            -webkit-transform: translateY(0px);
-            transform: translateY(0px);
-        }
+    
+    0%{
+        -webkit-transform: translateY(0px);
+        transform: translateY(0px);
+    }
+    50%{
+        -webkit-transform: translateY(-20px);
+        transform: translateY(-20px);
+    }
+    100%{
+        -webkit-transform: translateY(0px);
+        transform: translateY(0px);
+    }
     
     `
+    const animation = ()=>{
+        if(animated){
+            return flipAnimated?flipAnimation:successAnimation
+        }
+        return ''
+    }
     
     const ContainerBox = styled(Box)(() => ({
 
         width: '100%',
-        aspectRatio:'1/1',
+        aspectRatio: '1/1',
+        position:'relative',
         transformStyle: 'preserve-3d',
-        animation: flipAnimated&&`${flipAnimation} 0.7s ${delayTime}s linear both`,
-        animation: successAnimated&&`${successAnimation} 0.7s ${delayTime}s linear both`,
+        animation: animated?`${animation()} 0.7s ${delayTime}s linear both`:'',
         'z-index': 1,
 
     }))
 
-    const stageOne = ()=>{
-        return (
+    const StageOne = () => {
+        return stage===1?(
             <ContainerBox>
-                <EmptySquare/>
+                <EmptySquare />
             </ContainerBox>
-        )
+        ):<></>
     }
-    const stageTwo = ()=>{
-        return(
+    const StageTwo = () => {
+        return stage===2?(
             <ContainerBox>
-                <InputSquare key={key} value={null}/>
+                <InputSquare id={id} value={null} />
             </ContainerBox>
-        )
+        ):<></>
     }
-    const stageThree = ()=>{
-        return(
+    const StageThree = () => {
+
+        return stage===3?(
             <ContainerBox>
-                <InputSquare key={key} value={value[0]}/>
-                <FilledSquare stage={3} value={value[0]} success={value[1]} key={key}/>
+                <InputSquare id={id} value={value[0]}/>
+                <FilledSquare stage={stage} value={value[0]} success={value[1]}/>
             </ContainerBox>
-        )
+        ):<></>
     }
-    const stageFour = ()=>{
-        <ContainerBox>
-            <FilledSquare stage={4} value={value[0]} success={value[1]} key={key}/>
-        </ContainerBox>
+    
+    const StageFour = () => {
+
+        return stage===4?(
+            <ContainerBox>
+                <FilledSquare stage={stage} value={value[0]} success={value[1]}/>
+            </ContainerBox>
+        ):<></>
     }
 
 
-/*     const Container = styled(Box)(() => ({
-        
+    const Container = styled(Box)(() => ({
+
         position: 'relative',
         width: '100%',
-        aspectRatio:'1/1',
+        aspectRatio: '1/1',
         perspective: '250px',
 
-    })) */
+    }))
 
     return (
         <>
-            {stage===1&&<stageOne/>}
-            {stage===2&&<stageTwo/>}
-            {stage===3&&<stageThree/>}
-            {stage===4&&<stageFour/>}
+            <StageOne />
+            <StageTwo />
+            <StageThree />
+            <StageFour />
         </>
     )
 }
