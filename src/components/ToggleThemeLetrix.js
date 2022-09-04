2e1e766327a659c71de3d5f7d2b001b0b0bbd419
Igor Galvao <igorgalvaob@gmail.com>
2022-09-04 15:07:48 -0300

    AnimatedItem done

--- src/components/ToggleThemeLetrix.js
+++ src/components/ToggleThemeLetrix.js
@@ -1,21 +1,20 @@
 import Letrix from './Letrix.js';
-import Themes from './styles/Themes.js';
+import Themes from './ui/styles/Themes.js';
 import { createContext, useState, useMemo } from 'react';
-import  { createTheme, ThemeProvider }  from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 export const ThemeContext = createContext({})
 
 const ToggleThemeLetrix = () => {
     const [selectedTheme, setSelectedTheme] = useState(Themes.lightTheme)
-    
-    
+
     const toggleTheme = () => {
         setSelectedTheme((preTheme) => preTheme.palette.mode === 'dark' ? Themes.lightTheme : Themes.darkTheme)
     }
-    const  theme = createTheme(selectedTheme)
-        
-    
+    const theme = createTheme(selectedTheme)
+
+
     return (
         <ThemeContext.Provider value={toggleTheme}>
             <ThemeProvider theme={theme}>
